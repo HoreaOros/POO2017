0bc22cdbdb9d909f1ce790773d716855b891e7fc
Horea Oros <horea.oros@gmail.com>
2017-04-04 13:14:47 +0300

    Add project Cont

---
+++ ContBancar/ContBancar/Cont.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContBancar
+{
+    class Cont
+    {
+        private string titular;
+        private decimal sold;
+        private static int contor = 0;
+
+        public Cont(string titular): this(titular, 0)
+        {
+
+        }
+        public Cont(string titular, decimal sold)
+        {
+            this.titular = titular;
+            this.sold = sold;
+            contor++;
+        }
+        public static int Count
+        {
+            get
+            {
+                return contor;
+            }
+        }
+        public void Depune(decimal suma)
+        {
+            if (suma >= 0)
+            {
+                sold += suma;
+            }
+            else
+            {
+                throw new InvalidAmountException("suma depusa este negativa");
+            }
+        }
+    }
+}
---
+++ ContBancar/ContBancar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContBancar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cont c1 = new Cont("John Doe");
+            Cont c2 = new Cont("Jane Doe", 1000m);
+
+            
+            Console.WriteLine("Numarul total de conturi create este: {0}", Cont.Count);
+
+            c1.Depune(100m);
+            c1.Retrage(50m);
+            c1.Retrage(150m);
+        }
+    }
+}
